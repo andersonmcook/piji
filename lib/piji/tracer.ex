6d75be239183df3a729a43ecdccb39039e94f740
Anderson Cook <andersonmcook@gmail.com>
2021-09-01 22:19:09 -0500

    Enable/disable

--- lib/piji/tracer.ex
+++ lib/piji/tracer.ex
@@ -3,22 +3,16 @@ defmodule Piji.Tracer do
 
   use GenServer
 
-  defmodule Entry do
-    defstruct [:event, :from, :message, :serial, :to]
-
-    def new({event, serial, from, to, message}) do
-      %__MODULE__{
-        event: event,
-        from: from,
-        message: message,
-        serial: serial,
-        to: to
-      }
-    end
-  end
-
   def start_link(_) do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def disable do
+    GenServer.cast(__MODULE__, {:toggle, false})
+  end
+
+  def enable do
+    GenServer.cast(__MODULE__, {:toggle, true})
   end
 
   def state do
@@ -27,7 +21,6 @@ defmodule Piji.Tracer do
 
   @impl GenServer
   def init(_) do
-    :seq_trace.set_system_tracer(self())
     {:ok, %{}}
   end
 
@@ -37,13 +30,30 @@ defmodule Piji.Tracer do
   end
 
   @impl GenServer
-  def handle_info({:seq_trace, _label, seq_trace_info, timestamp}, state) do
-    # Naively assuming that no events happen at the same time
-    {:noreply, Map.put(state, timestamp, Entry.new(seq_trace_info))}
+  def handle_cast({:toggle, boolean}, state) do
+    tracer = if boolean, do: self(), else: false
+    :seq_trace.set_system_tracer(tracer)
+    {:noreply, state}
   end
 
+  @impl GenServer
+  def handle_info({:seq_trace, _label, seq_trace_info, timestamp}, state) do
+    # Naively assuming that no events happen at the same time
+    {:noreply, add_entry(state, timestamp, seq_trace_info)}
+  end
+
+  # Received message with no timestamp
   def handle_info({:seq_trace, _label, _seq_trace_info}, state) do
-    IO.puts("received message with no timestamp")
     {:noreply, state}
+  end
+
+  defp add_entry(state, key, {event, serial, from, to, message}) do
+    Map.put(state, key, %{
+      event: event,
+      from: from,
+      message: message,
+      serial: serial,
+      to: to
+    })
   end
 end
